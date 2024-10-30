06c5350a88c77192ee551f3d8c8da74a670812f6
universe119 <ruddh119@gmail.com>
2024-10-30 09:19:46 +0900

    검색요청에 대한 url과 view맵핑, 게시글 역순 출력 추가

--- post_app/views.py
+++ post_app/views.py
@@ -4,6 +4,7 @@ from post_app.serializers import PostSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.db.models import Q
 
 # 순서2 - url요청이 들어왔을때 요청의 HTTP방식을 확인해서 해당 방식에 맞는 DB 데이터(모델) 제어
 @api_view(['GET', 'POST'])
@@ -11,7 +12,7 @@ def posts(request):
 
 # 들어온 요청이 GET방식일때는 기존의 모델데이터를 JSON형태로 직렬화해서 프론트로 전달
   if request.method == 'GET':
-    posts = Posts.objects.all()
+    posts = Posts.objects.all().order_by("-id")
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
   
@@ -59,4 +60,15 @@ def posts_detail(request, slug):
     post.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
 
-      
+@api_view(['GET'])
+def posts_search(request):
+  # 프론트쪽 url요청시 search란 항목의 params 쿼리정보 가져옴
+  query = request.query_params.get("search")
+  
+  # Q메서드를 이용해 title필드에서 대소문자 구분없이 query가 포함된 문자값이 있으면 true반환 없으면 false반환
+  # 아래 코드 해석 클라이언트로부터 전달된 쿼리정보가 title필드 항목에 있거나 혹은 body필드 항목에 있거나 혹은 category필드 항목에 매칭되는게 하나라도 있으면 해당 모델을 모두 필터링해서 반환
+  posts = Posts.objects.filter(Q(title__icontains=query) | Q(body__icontains=query) | Q(category__icontains=query))
+  
+  serializer = PostSerializer(posts, many=True)
+  
+  return Response(serializer.data, status=status.HTTP_200_OK)
