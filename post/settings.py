b92047229ef86145968383be7d30d600e57c2c9b
universe119 <ruddh119@gmail.com>
2024-11-04 22:56:27 +0900

    url 재수정

--- post/settings.py
+++ post/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b++s_@^l-d$_l@#bbj%lsu8=@bqk+va-(0bgih&2k6@c3r=-nf
 DEBUG = True
 
 # Render 서비스에서 배포 후 등록한 url
-ALLOWED_HOSTS = ["localhost","psyh-hotelcommunitydb.onrender.com","https://group-b-hotel.vercel.app/"]
+ALLOWED_HOSTS = ["localhost","psyh-hotelcommunitydb.onrender.com","group-b-hotel.vercel.app"]
 
 # Application definition
 # 만약 추가되는 라이브러리나 앱이 있을시에 무조건 아래쪽에 등록
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://group-b-hotel.vercel.app/",
+    "https://group-b-hotel.vercel.app",
 ]
 
 ROOT_URLCONF = 'post.urls'
